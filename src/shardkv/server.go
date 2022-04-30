b8b8441dc63aa380891321f6b164f904f4d6a8fa
jingyusu <gar123su@gmail.com>
2022-04-30 00:43:22 -0400

    server struct partial update

--- src/shardkv/server.go
+++ src/shardkv/server.go
@@ -1,12 +1,9 @@
 package shardkv
 
-
 import "6.824/labrpc"
 import "6.824/raft"
 import "sync"
 import "6.824/labgob"
-
-
 
 type Op struct {
 	// Your definitions here.
@@ -25,8 +22,9 @@ type ShardKV struct {
 	maxraftstate int // snapshot if log grows this big
 
 	// Your definitions here.
-}
+	lastApplied int //last applied index, for log compaction
 
+}
 
 func (kv *ShardKV) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
@@ -46,7 +44,6 @@ func (kv *ShardKV) Kill() {
 	kv.rf.Kill()
 	// Your code here, if desired.
 }
-
 
 //
 // servers[] contains the ports of the servers in this group.
@@ -95,7 +92,6 @@ func StartServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persister,
 
 	kv.applyCh = make(chan raft.ApplyMsg)
 	kv.rf = raft.Make(servers, me, persister, kv.applyCh)
-
 
 	return kv
 }
